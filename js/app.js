a55072a165e20dda417b47e02f0969bf4b44f7fa
nikosaja99 <projectniko153@gmail.com>
2026-06-18 13:53:14 +0700

    Update app.js

--- js/app.js
+++ js/app.js
@@ -1,28 +1,15 @@
 document.addEventListener("DOMContentLoaded", () => {
 
-  console.log("APP JALAN");
-
   const profile = getData("mymoney_profile");
 
-  console.log("PROFILE:", profile);
-
-  console.log("PATH:", window.location.pathname);
-
-  const path = window.location.pathname;
-
-  const isHomePage =
-    path.endsWith("/") ||
-    path.endsWith("index.html");
-
-  console.log("IS HOMEPAGE:", isHomePage);
-
-  if (isHomePage) {
+  if (
+    window.location.pathname.includes("index.html") ||
+    window.location.pathname === "/page"
+  ) {
 
     if (profile) {
-      console.log("KE DASHBOARD");
       window.location.href = "pages/dashboard.html";
     } else {
-      console.log("KE SETUP");
       window.location.href = "pages/setup.html";
     }
 
